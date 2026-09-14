ebb01cd1eea763d9a8074f549591e2a318a244e6
aviii1229 <aviral9b19@gmail.com>
2026-09-14 13:09:36 +0530

    fix(map): replace Carto with Esri Dark Gray canvas to remove API key required watermark

--- map/mapConfig.js
+++ map/mapConfig.js
@@ -28,11 +28,11 @@
         id: 'tactical',
         name: 'Tactical Dark Mode',
         label: '🌙 Dark Mode',
-        url: 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
-        subdomains: 'abcd',
-        maxNativeZoom: 19,
+        url: 'https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Dark_Gray_Base/MapServer/tile/{z}/{y}/{x}',
+        subdomains: '',
+        maxNativeZoom: 16,
         maxZoom: 20,
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright" target="_blank">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions" target="_blank">CARTO</a>'
+        attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ'
       },
       topo: {
         id: 'topo',
